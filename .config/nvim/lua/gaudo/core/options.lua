0fd861842e78d07ee65adb7165304af146ec4ff7
Francesco Gaudenzi <fgaudo@gmail.com>
2023-12-23 23:39:22 +0100

    dev

--- .config/nvim/lua/gaudo/core/options.lua
+++ .config/nvim/lua/gaudo/core/options.lua
@@ -4,7 +4,11 @@ opt.relativenumber = true
 opt.number = true
 
 opt.tabstop = 3
+opt.softtabstop = 3
+opt.shiftwidth = 3
 opt.wrap = false
+
+opt.smartindent = true
 
 opt.termguicolors = true
 opt.background = "dark"
@@ -14,5 +18,13 @@ opt.swapfile = false
 opt.backup = false
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.scrolloff = 8
+opt.updatetime = 50
+
+--opt.colorcolumn = "80"
 
 vim.cmd("syntax off")
