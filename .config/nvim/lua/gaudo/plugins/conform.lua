72dd1b77821bf54989d8576c981fbecc63351452
Francesco Gaudenzi <fgaudo@gmail.com>
2023-12-24 21:58:19 +0100

    dev

---
+++ .config/nvim/lua/gaudo/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 10000, lsp_fallback = true },
+		-- Customize formatters
+
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+}
